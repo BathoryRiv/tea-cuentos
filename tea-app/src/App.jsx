9625b5ff64c1a2003928e5313ff5502e4f5f2895
Gabriela Rivera <gabrielarivera@MacBook-Air-de-Gabriela-3.local>
2023-08-07 18:33:16 -0600

    first commit

---
+++ tea-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='portada'>
+    <img src='../public/cuento-imagenes/portada.svg'/>
+    </div>
+    
+  )
+}
+
+export default App
