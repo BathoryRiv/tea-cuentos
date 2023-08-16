716771f8ceb6fe1aaaec1e688f3a79fb63a3b57d
Gabriela Rivera <gabrielarivera@MacBook-Air-de-Gabriela-3.local>
2023-08-15 18:43:53 -0600

    style: 🎨 edited MenuPage cards

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -7,8 +7,8 @@ export const MenuPage = () => {
         <img className='title' src={"../../public/cuento-imagenes/Elige un cuento.svg"}  /> 
         <div className="button-container">
             <MenuButton card={'tessi'} icon={"./../../public/cuento-iconos/icono tessi.svg"} name={'Tessi'}/>
-            <MenuButton card={'elio'} icon={"./../../public/cuento-iconos/icono tessi.svg"} name={'Elio'}/>
-            <MenuButton card={'ale'} icon={"./../../public/cuento-iconos/icono tessi.svg"} name={'Ale'}/>
+            <MenuButton card={'elio'} icon={"./../../public/cuento-iconos/icono elio.svg"} name={'Elio'}/>
+            <MenuButton card={'ale'} icon={"./../../public/cuento-iconos/icono ale.svg"} name={'Ale'}/>
         </div>
     </div>
    
